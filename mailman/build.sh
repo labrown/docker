1380fe76672c67d49564632e055ef008de6b7fa9
Matt Lehner <matt@foundry.xavia.org>
2014-02-24 02:04:45 +0000

    fix build process for mailman

--- mailman/build.sh
+++ mailman/build.sh
@@ -6,4 +6,8 @@ set -x
 # Install mailman and dependencies
 $minimal_apt_get_install mailman postfix spawn-fcgi
 
+# Enable mailman service
 cp -R /build/service-files/mailman /etc/service
+
+# Enable postfix service
+cp -R /build/service-files/postfix /etc/service
